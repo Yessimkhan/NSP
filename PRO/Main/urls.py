5f6c5382a135cebcaea8e6ddd01c7b6d8a4ad6ae
Yessimkhan <200103408@stu.sdu.edu.kz>
2022-06-18 23:06:34 +0600

    sign up and sign in
    up and in

---
+++ PRO/Main/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+app_name = "main"
+
+urlpatterns = [
+    path("", views.Main, name="homepage"),
+    path("register", views.register_request, name="register"),
+    path("login", views.login_request, name="login")
+]
--- PRO/PRO/settings.py
+++ PRO/PRO/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms',
     'Main',
 ]
 
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'PRO.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Smile',
+        'USER': 'postgres',
+        'PASSWORD': '0022',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
